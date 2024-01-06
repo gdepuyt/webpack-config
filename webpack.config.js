a10a37675158bee9abd9373e5f641cb470185ad0
Geoffroy De Puyt <geoffroy.depuyt@gmail.com>
2024-01-06 14:49:18 +0100

    add webpack clean & webpackhtml

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require('node:path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -8,18 +13,29 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
     mode: mode,
-    module:{
-        rules:[{
-            test:/\.js$/,
-            exclude: /node_modules/,
-            use:{
-                loader:"babel-loader",
+    output: { path: path.resolve(__dirname, "dist") },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
 
-        },],
+            },],
     },
-    devtool:"source-map",
-    devServer:{
-    static: "./dist"
-},
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({ template: "./src/index.html" })],
+    devtool: "source-map",
+    devServer: {
+        static: "./dist"
+    },
 };
